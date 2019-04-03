5243443040890a14f5c0771a6e4796f28071cf62
ruzor <36074382+ruzor@users.noreply.github.com>
2019-04-03 12:24:06 +0100

    Add files via upload

--- Welcome/welcome.js
+++ Welcome/welcome.js
@@ -1,104 +1,254 @@
-document.querySelector('#reg-btn').addEventListener('submit', addUser());
+function _(str) {
+    return document.querySelector(str);
+}
 
-function addUser() {
-    console.log('hello');
-    // console.log(username, email, pwd, confirmPwd);
+const items = document.querySelectorAll('.char');
+const links = document.querySelectorAll("a[href='#'] span");
 
-    // let username = document.getElementById('#usr').value;
-    // let email = document.getElementById('#reg-email').value;
-    // let pwd = document.getElementById('#reg-pwd').value;
-    // let confirmPwd = document.getElementById('#reg-pwd-confirm').value;
 
-    let username = '';
-    let email = '';
-    let pwd = '';
-    let confirmPwd = '';
+// animate random letters
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://backtick.herokuapp.com/api/register', true);
-    
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    xhr.onload = function() {
-        if (this.status == 200) {
-            console.log("hello");
-        };
-    };
-    xhr.onerror = function() {
-        console.log('Error...');
+function animaterandom() {
+intervalrandom = setInterval(() => {
+    let i = Math.floor(Math.random() * 100);
+    if (i > 84) {
+    i-=15;
     }
-    xhr.send(username, email, pwd, confirmPwd);
+    items[i].classList.add('animate');
+    items[i].addEventListener("animationend", () => {
+    items[i].classList.remove('animate');
+    })
+}, 5000);
 };
 
-let sidebarBox = document.querySelector('#form-user');
-let joinBtn = document.querySelector('#btn-home');
-let pageWrapper = document.querySelector('#container');
-let login = document.querySelector('#login');
-let register = document.querySelector('#register');
-let btnRegister = document.querySelector('#register-head');
-let btnLogin = document.querySelector('#login-head');
-let overlay = document.querySelector('#overlay');
 
-joinBtn.addEventListener('click', function(event) {
+// clear animation on random letters
 
-    if (this.classList.contains('active')) {
-      this.classList.remove('active');
-      sidebarBox.classList.remove('active');
-      login.classList.remove('active');
-      overlay.classList.remove('active');
-  } else {
-      this.classList.add('active');
-      sidebarBox.classList.add('active');
-      login.classList.add('active');
-      btnLogin.classList.add('active');
-      overlay.classList.add('active');
-  }
-});
+function clear() {
+    clearInterval(intervalrandom);  
+}
 
-// pageWrapper.addEventListener('click', function(event) {
-//   if (sidebarBox.classList.contains('active')) {
-//       joinBtn.classList.remove('active');
-//       sidebarBox.classList.remove('active');
-//   }
-// });
 
-window.addEventListener('keydown', function(event) {
+// IIFE to animate links
 
-  if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
-      joinBtn.classList.remove('active');
-      sidebarBox.classList.remove('active');
-      register.classList.remove('active');
-      login.classList.add('active');
-      overlay.classList.remove('active');
-  }
-});
+(function animatelinks() {
 
-// login/signup scope
+    interval = setInterval(() => {
+        clear();
+        links.forEach((e) => { 
+        e.classList.add('animated', 'flash');
+        e.addEventListener("animationend", () => {
+            e.classList.remove('animated', 'flash');
+        })
+        });
+    }, 10000);
+    animaterandom();
+})();
 
-btnLogin.addEventListener('click', function(event) {
-    if (this.classList.contains('active')) {
-        // clear user input
-    } else {
-        this.classList.add('active');
-        login.classList.add('active');
-        register.classList.remove('active');
-        btnRegister.classList.remove('active');
-    }
+
+// event listeners to animate letters on mouseenter, and end animation
+
+items.forEach((e) => {
+    e.addEventListener("animationend", () => {
+        e.classList.remove('animate');
+    })
 })
-btnRegister.addEventListener('click', function(event) {
-    if (this.classList.contains('active')) {
-        // clear user input
-    } else {
-        this.classList.add('active');
-        register.classList.add('active');
-        login.classList.remove('active');
-        btnLogin.classList.remove('active');
-    }
-})
-window.addEventListener('keydown', function(event) {
 
-    if (btnRegister.classList.contains('active') && event.keyCode === 27) {
-        btnRegister.classList.remove('active');
+items.forEach((e) => {
+    e.addEventListener('mouseover', () => {
+        e.classList.add('animate');
+    })
+})
+
+
+// call registration api
+
+function addUser(iti) {
+
+    let username = _('#user').value;
+    let email = _('#reg-email').value;
+    let phony = iti.getNumber();
+    let pwd = _('#reg-pwd').value;
+    let cpwd = _('#reg-pwd-confirm').value;
+    let user = {
+        username: username,
+        email: email,
+        phone: phony,
+        password: pwd,
+        password_confirmation: cpwd
     }
-});
-  
+    let url = 'http://backtick.herokuapp.com/public/api/register';
+
+    axios.post(url, user).then((response) => {
+        console.log(response.data);
+        const token = "Bearer " + response.data.user.api_token;
+        localStorage.setItem('sessionToken', token);
+        location.replace('../dashboard/dashboard.html');
+    }).catch((err) => {
+        console.log(err.response);
+    });
+}
+
+
+// call login api
+
+function logUser() {
+
+    let email = _('#login-email').value;
+    let pwd = _('#login-pwd').value;
+    let user = {
+        email: email,
+        password: pwd
+    }
+    let url = 'http://backtick.herokuapp.com/public/api/login';
+    
+    axios.post(url, user)
+    .then((response) => {
+        const token = "Bearer " + response.data.token;
+
+        localStorage.setItem('sessionToken', token);
+        location.replace("../dashboard/dashboard.html");
+    }).catch((err) => {
+        console.log(err.response);
+    });
+}
+
+
+// show login form
+
+function loginUI() {
+    _('.form').innerHTML = `<div class="logo">
+            <span class="logo-text">B</span> 
+            <span class="logo-text">A</span> 
+            <span class="logo-text">C</span> 
+            <span class="logo-text">K</span> 
+            <span class="logo-text">T</span> 
+            <span class="logo-text">I</span> 
+            <span class="logo-text">C</span> 
+            <span class="logo-text">K</span>
+        </div>
+        <div class="wrapper-login">
+            <h1 id="login-head">
+                Login to unlock potential
+            </h1>
+            
+            <input id="login-email" name="login-email" type="email" value="" placeholder="Email" required>
+
+            <input id="login-pwd" name="login-pwd" type="password" value="" placeholder="Password" required>
+            
+            <input id="login-btn" type="submit" value="Continue">
+
+            <small class="small-text">
+                Not a member? <a href="#">Get an account</a>
+            </small>
+        </div>
+        <small id="bottom">
+            a venture by the fine fellas at team mexico. drink responsibly 🍺 ♠
+        </small>`
+        
+    if (!(_('.wrapper-login').classList.contains('active'))) {
+        _('.form').addEventListener('submit', logUser);
+    }
+
+    // link to signup
+    _('.wrapper-login a').addEventListener('click', () => {
+        signUpUI();
+    })
+}
+
+
+// show signUp form
+
+function signUpUI() {
+    _('.form').innerHTML = `<div class="logo">
+            <span class="logo-text">B</span> 
+            <span class="logo-text">A</span> 
+            <span class="logo-text">C</span> 
+            <span class="logo-text">K</span> 
+            <span class="logo-text">T</span> 
+            <span class="logo-text">I</span> 
+            <span class="logo-text">C</span> 
+            <span class="logo-text">K</span>
+        </div>
+        <div class="wrapper-signup">
+            <h1 id="login-head">
+                Signup to unlock potential
+            </h1>
+            <small class="small-text">
+                // Access Backtick with a free account
+            </small>
+            
+            <input id="user" name="usr" type="text" placeholder="Username" required>
+
+            <input id="reg-email" name="reg-email" type="email" placeholder="Email" required>
+
+            <input id="reg-phone" name="reg-phone" type="tel" placeholder="" required>
+
+            <input id="reg-pwd" name="reg-pwd" type="password" placeholder="Password" required>
+
+            <input id="reg-pwd-confirm" name="reg-pwd-confirm" type="password" placeholder="Confirm password" required>
+            
+            <input id="reg-btn" type="submit" value="Continue">
+            <small class="small-text">
+                Already a member? <a href="#">Login</a>
+            </small>
+        </div>
+        <small id="bottom">
+            a venture by the fine fellas at team mexico. drink responsibly 🍺 ♠
+        </small>`
+
+    let phone = _("#reg-phone");
+
+    // init intl phone api
+    let iti = window.intlTelInput(phone, {
+        hiddenInput: "full-phone",
+        nationalMode: true,
+        utilsScript: "../assets/apps/intl-tel-input-15.0.0/build/js/utils.js?1549804213570"
+    });
+
+    if (!(_('.wrapper-signup').classList.contains('active'))) {
+        _('.form').addEventListener('submit', () => {
+            addUser(iti);
+        });
+    }
+
+    // link to login
+    _('.wrapper-signup a').addEventListener('click', () => {
+        loginUI();
+    })    
+}
+
+
+// link to login form
+
+_('#link-login').addEventListener('click', () => {
+
+    _('#con-flex').classList.add('active');
+    loginUI();
+    _('.form').classList.add('active', 'animated', 'fadeIn');
+    _('.form').addEventListener("animationend", () => {
+        _('.form').classList.remove('animated', 'fadeIn');
+    })
+})
+
+
+// link to signup form
+
+_('#link-signup').addEventListener('click', () => {
+
+    _('#con-flex').classList.add('active');
+    signUpUI();
+    _('.form').classList.add('active', 'animated', 'fadeIn');
+    _('.form').addEventListener("animationend", () => {
+        _('.form').classList.remove('animated', 'fadeIn');
+    })
+})
+
+window.addEventListener('keydown', () => {
+
+    if (_('#con-flex').classList.contains('active') && event.keyCode === 27) {
+        _('.form').classList.remove('active');
+        _('#con-flex').classList.remove('active');
+    }
+});  
+
